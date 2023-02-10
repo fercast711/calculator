17afba219395d8dadbe3fae54f9f081f35550c02
fercast711 <fercast7.fc@gmail.com>
2023-02-09 22:22:07 -0600

    update1

--- src/components/Convertidor.js
+++ src/components/Convertidor.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch,useSelector } from 'react-redux'
 import { setDecimal, setRoman } from '../store/slice/coverterSlice';
 const Convertidor = () => {
+  const [input,setInput] = useState('')
   const {roman} = useSelector(state=>state.convert)
   const dispatch = useDispatch();
   return (
@@ -12,19 +13,21 @@ const Convertidor = () => {
                 <h3>Convert Decimal To Roman</h3>
             </div>
             <div className='card-body'>
-              <input type="number" className='form-control' id="decimal" placeholder='Decimal Number' min="1" max="4999"/>
+              <input type="number" className='form-control' id="decimal" placeholder='Decimal Number' min="1" max="4999" value={input} onChange={(e)=>{
+                setInput(e.target.value)
+              }}/>
               <br/>
               <input type="text" value={roman} className='form-control' placeholder='Roman Number' readOnly/>
               <hr/>
               <div id='boton-main'>
                 <button className='btn btn-outline-primary' onClick={()=>{
-                  if(Number(document.getElementById("decimal").value) >0 && Number(document.getElementById("decimal").value) < 5000){
-                    dispatch(setDecimal(Number(document.getElementById("decimal").value)))
+                  if(Number(input) >0 && Number(input) < 5000){
+                    dispatch(setDecimal(Number(input)))
                     dispatch(setRoman())
                   }else{
                     alert("The number must be more to 0 and less to 5000")
-                  }
-                  
+                  } 
+                    
                 }
                 }>Convert</button>
               </div>
